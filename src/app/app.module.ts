f13a189b73f0f4084aea3f94d65b20cc51619657
Celie Baker <celie.n133@gmail.com>
2018-06-20 10:39:43 -0600

    elite schdule app

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GamePage } from "../pages/game/game";
+import { MyTeamsPage } from "../pages/my-teams/my-teams";
+import { TeamDetailPage } from "../pages/team-detail/team-detail";
+import { TeamsPage } from "../pages/teams/teams";
+import { TournamentsPage } from "../pages/tournaments/tournaments";
+import { StandingsPage } from "../pages/standings/standings";
+import { TeamHomePage } from "../pages/team-home/team-home";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    GamePage,
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    TournamentsPage,
+    StandingsPage,
+    TeamHomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    GamePage,
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    TournamentsPage,
+    StandingsPage,
+    TeamHomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/teams/teams.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TeamHomePage } from "../team-home/team-home";
+
+/**
+ * Generated class for the TeamsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-teams',
+  templateUrl: 'teams.html',
+})
+export class TeamsPage {
+  teams = [
+    { id: 1, name: 'HC Elite' },
+    { id: 2, name: 'Team Takeover' },
+    { id: 3, name: 'DC Thunder' }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TeamsPage');
+  }
+
+  goToTeamHome(event, team) {
+    this.navCtrl.push(TeamHomePage, {
+      'team': team
+    });
+  }
+}
